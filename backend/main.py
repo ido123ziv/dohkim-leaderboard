2aec83bd7d1f88c94e9ef25c836e51b569986d0f
roiy <roiy355@gmail.com>
2022-12-21 14:45:42 +0200

    init server needed to fix docker integration

--- backend/main.py
+++ backend/main.py
@@ -1,8 +1,10 @@
 import psycopg2
 import os
+from blacksheep import Application
+
 # from dotenv import load_dotenv
 # load_dotenv()
-QUERY="SELECT * from winners"
+QUERY = "SELECT * from winners"
 # print(os.environ.get('DATABASE_URL'))
 # print(os.environ.get('DATABASE_NAME'))
 # print(os.environ.get('DATABASE_USERNAME'))
@@ -13,6 +15,21 @@ conn = psycopg2.connect(
     user=os.environ.get('DATABASE_USERNAME'),
     password=os.environ.get('DATABASE_PASSWORD'))
 
-cur = conn.cursor()
-results = cur.execute(QUERY)
-print(f"the results are here!\n{results}")
+# cur = conn.cursor()
+# results = cur.execute(QUERY)
+# print(f"the results are here!\n{results}")
+
+
+app = Application()
+get = app.router.get
+post = app.router.post
+
+
+@get("/")
+def home(request):
+    return "GET Example"
+
+
+@post("/")
+def post_example(request):
+    return "POST Example"

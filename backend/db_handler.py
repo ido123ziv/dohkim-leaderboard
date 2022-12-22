873add946a2047c73d9960f9eb31a3188245f98e
idosh <idoshziv@gmail.com>
2022-12-22 09:49:55 +0200

    documentation

--- backend/db_handler.py
+++ backend/db_handler.py
@@ -3,6 +3,8 @@ import os
 
 # from dotenv import load_dotenv
 # load_dotenv()
+
+# SQL Queries
 QUERY = "SELECT * from winners"
 INSERT = """INSERT into winners (winner_name) values ('ido')"""
 conn = psycopg2.connect(
@@ -11,7 +13,9 @@ conn = psycopg2.connect(
     user=os.environ.get('DATABASE_USERNAME'),
     password=os.environ.get('DATABASE_PASSWORD'))
 
-
+"""
+returns all the data from winners table
+"""
 def get_data():
     cur = conn.cursor()
     cur.execute(QUERY)
@@ -19,7 +23,9 @@ def get_data():
     print(f"the results are here!\n{results}")
     return results
 
-
+"""
+adds another winner to db
+"""
 def update_data():
     cur = conn.cursor()
     cur.execute(INSERT)
